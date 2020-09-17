a2483a997f9c7154eaa9f3fc4a6605beaf9bebaf
Bryson Bargar <bbargar@clemson.edu>
2020-09-17 13:54:03 -0400

    movie list

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,13 +1,21 @@
 import urllib.request
 import json
 import sys
-
+import os
 
 api_key = sys.argv[1]
 
+if not os.path.exists('json_files'):
+	os.mkdir('json_files')
+
 #print(api_key)
 
+#movie_count = int(json_response['id'])
+#print(movie_count)
 
+#movie_start = movie_count -5
+
+movie_id = '550'
 response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/550?api_key=' + api_key)
 json_response = json.load(response)
 
