fc6a7b739322c658739c1a30c12bf57d5cf84a71
Bryson Bargar <bbargar@clemson.edu>
2020-09-22 12:59:29 -0400

    added movie_id request

--- tmdb_request.py
+++ tmdb_request.py
@@ -10,15 +10,17 @@ if not os.path.exists('json_files'):
 
 #print(api_key)
 
+#response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/550?api_key=' + api_key)
+#json_response = json.load(response)
+
 #movie_count = int(json_response['id'])
 #print(movie_count)
 
 #movie_start = movie_count -5
 
 movie_id = '550'
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/550?api_key=' + api_key)
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
 json_response = json.load(response)
-
 print(json_response)
 
 
