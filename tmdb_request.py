5ed7250e12c917f6f19d1b6e0d4f818d0fef3042
Bryson Bargar <bbargar@clemson.edu>
2020-09-29 12:08:20 -0400

    request and parse

--- tmdb_request.py
+++ tmdb_request.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 import sys
 import os
+import time
 
 api_key = sys.argv[1]
 
@@ -10,17 +11,26 @@ if not os.path.exists('json_files'):
 
 #print(api_key)
 
-#response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/550?api_key=' + api_key)
-#json_response = json.load(response)
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+json_response = json.load(response)
 
-#movie_count = int(json_response['id'])
+movie_count = int(json_response['id'])
 #print(movie_count)
 
-#movie_start = movie_count -5
+movie_start = movie_count-30
+#movie_start = 1 (this would go through every movie. -5 would take the latest 5 movies)
 
-movie_id = '550'
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
-json_response = json.load(response)
-print(json_response)
+
+for i in range(movie_start,movie_count): 
+	print(i)
+	movie_id = str(i) 
+	response = urllib.request.urlopen('http://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+	json_response = json.load(response)
+	#print(json_response)
+	f = open("json_files/tmdb" + movie_id + ".json", "w")
+	f.write(json.dumps(json_response))
+	f.close()
+	time.sleep(15)
+
 
 
